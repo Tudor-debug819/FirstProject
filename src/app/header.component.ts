496e4de541d9427280caa87f4b71939d71e76a25
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-11-30 23:19:23 +0200

    test

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    standalone: true, //if use a version lower than 19 you have to set it to true/ is the modern way to built angular 
+    // standalone: true, //if use a version lower than 19 you have to set it to true/ is the modern way to built angular 
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{}
