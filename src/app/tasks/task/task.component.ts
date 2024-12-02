b09244bd73d31d6314f7da46491b76caaaf3a1d4
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-02 10:17:43 +0200

    adding different tasks

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
-export class TaskComponent {
+export class TaskComponent {  
+
 
 }
